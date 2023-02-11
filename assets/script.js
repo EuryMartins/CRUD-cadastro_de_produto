3d7121ea888147571e62688e75f9756adaf0b20c
EuryMartins <104109064+EuryMartins@users.noreply.github.com>
2023-02-11 19:50:18 -0400

    atualizado javascript

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
-function iniciaModal(modalID) {
-    const modalStart = document.getElementById(modalID);
+function iniciaModal(modalST) {
+    const modalStart = document.getElementById(modalST);
     modalStart.classList.add('mostrar')
 }
 
@@ -8,4 +8,14 @@ cadastrar.addEventListener('click', function(){
     iniciaModal('modal');
 })
 
+
+function fechaModal(modalCL) {
+    const modalStart = document.getElementById(modalCL);
+    modalStart.classList.add('ocutar')
+}
+
+const fechar = document.querySelector('.bt-fechar')
+fechar.addEventListener('click', function(){
+    fechaModal('modal');
+})
 
